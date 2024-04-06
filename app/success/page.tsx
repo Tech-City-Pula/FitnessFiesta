4e9b63f968b08ee3ebd7b00cc9cf9f946ecd4ceb
voiys <matej2714@gmail.com>
2024-04-06 11:41:22 +0200

    add success page

---
+++ app/success/page.tsx
@@ -0,0 +1,9 @@
+import { SuccessCard } from '@/components/SuccessCard';
+
+export default function SuccessPage() {
+	return (
+		<div className='w-screen h-screen flex items-center justify-center bg-blue-600'>
+			<SuccessCard />
+		</div>
+	);
+}
